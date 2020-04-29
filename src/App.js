e01fb7bfc67f62034d636ac9b8ea7629e1b5a224
SuvojitBarick <suvojitbarick135@gmail.com>
2020-04-29 20:16:26 +0530

    make it better

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ class App extends React.Component {
             params: {
                 part: 'snippet',
                 maxResults: 5,
-                key: '[API KEY]',
+                key: 'AIzaSyCL8uwvYhmDjMQZu5aijCpXufyNRPHiL6Y',
                 q: searchTerm,
             }
             });
